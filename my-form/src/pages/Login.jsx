f116c0517a7dc704d21dd50f02554926244cd207
Victor Ogunwehin <victorogunwehin@gmail.com>
2024-05-24 16:21:37 +0100

    first

---
+++ my-form/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <form>
+                    <h2>BRAND</h2>
+                    <div>
+                        <label>Username</label>
+                        <input type="text"></input>
+                    </div>
+                    <div>
+                        <label></label>
+                        <input type="password"></input>
+                    </div>
+                    <button>Sign In</button>
+                    <div>
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
